a50326211cf6fd3a2ddc33ddd33ef202844db3ab
Pyunghyuk Yoo <yoophi@gmail.com>
2021-09-03 00:12:26 +0900

    ✨ NEW: add Greeting component to shared app

---
+++ packages/shared/src/Greeting.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface GreetingProps {
+  whom?: string;
+}
+
+const Greeting: React.FC<GreetingProps> = ({ whom = "World" }) => {
+  return <h1>Hello {whom}</h1>;
+};
+
+export default Greeting;
