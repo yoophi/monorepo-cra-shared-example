249bd60ab353d92e62a6afba206c9a76918c37b0
Pyunghyuk Yoo <yoophi@gmail.com>
2021-09-03 00:22:48 +0900

    ✨ NEW: apply shared/Greeting component to project1

--- packages/project1/src/App.tsx
+++ packages/project1/src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Greeting } from "@my/shared";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Greeting />
       </header>
     </div>
   );
